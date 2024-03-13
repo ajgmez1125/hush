b1ae24e86922163b58ad00174a6d7538997571ce
Ayden <ajgg1125@gmail.com>
2024-03-13 16:20:49 -0400

    starting production

---
+++ hush-front/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/hello-world/')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
